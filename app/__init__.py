fcb631f15f01b0afc4e1a7bb813b8ce23253731f
Diana Zawislak <diana.zawislak@icloud.com>
2022-05-18 00:35:01 -0400

    added geolocation and js for interest rate

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,11 @@
 """A simple flask web app"""
 import os
 from flask import Flask, render_template
-
-from app import IBM_pages
-from app.cli import create_database
-from app.db import db
-from app.db.models import User
+from flask import Flask
+from flask_bootstrap import Bootstrap5
+from flask_wtf.csrf import CSRFProtect
+from app.context_processors import utility_text_processors
+from app.ibm import ibm
 
 
 def page_not_found(e):
@@ -15,19 +15,12 @@ def create_app():
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__)
     app.secret_key = 'This is an INSECURE secret!! DO NOT use this in production!!'
-
+    bootstrap = Bootstrap5(app)
     app.register_error_handler(404, page_not_found)
     db_dir = "database/db.sqlite"
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.abspath(db_dir)
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    db.init_app(app)
-    # add command function to cli commands
-    app.cli.add_command(create_database)
-    app.register_blueprint(IBM_pages)
+    app.register_blueprint(ibm)
 
-
-    @app.route('/')
-    def home():
-        return '/'
 
     return app
--- app/ibm/__init__.py
+++ app/ibm/__init__.py
@@ -2,29 +2,35 @@ from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
 
 
-IBM_pages = Blueprint('ibm_pages', __name__,
+ibm = Blueprint('ibm', __name__,
                         template_folder='templates')
 
 
-@IBM_pages.route('/')
+@ibm.route('/')
 def home():
     try:
         return render_template('index.html')
     except TemplateNotFound:
         abort(404)
 
-@IBM_pages.route('/form')
-def about():
+@ibm.route('/form')
+def form():
     try:
         return render_template('form.html')
     except TemplateNotFound:
         abort(404)
 
-@IBM_pages.route('/interestRate')
-def welcome():
+@ibm.route('/interestRate')
+def interestRate():
     try:
         return render_template('interestRate.html')
     except TemplateNotFound:
         abort(404)
 
+@ibm.route('/geolocation')
+def geolocation():
+    try:
+        return render_template('geolocation.html')
+    except TemplateNotFound:
+        abort(404)
 
